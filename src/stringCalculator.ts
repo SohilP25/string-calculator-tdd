2578558eca7825f5ce7235dfb97cfed24c92dd22
SohilP25 <srpathan254@gmail.com>
2024-05-26 17:13:57 +0530

    formatting code

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -13,10 +13,10 @@ class Calculator {
 
   add(): number {
     /*
-    ** @description : This function will calculate the sum of the numbers in the string
-    ** @param : input_str : string - Input must be in string
-    ** @return : number - Sum of number as output will be an Integer
-    */
+     ** @description : This function will calculate the sum of the numbers in the string
+     ** @param : input_str : string - Input must be in string
+     ** @return : number - Sum of number as output will be an Integer
+     */
 
     // Removing white spaces
     this.input_str = this.input_str.replace(/ /g, "");
@@ -26,43 +26,39 @@ class Calculator {
       return 0;
     }
 
-    let delimiter: string= ",";
+    let delimiter: string = ",";
 
     //si = start index, ei = end index
-    let si: number= this.input_str.indexOf("//");
+    let si: number = this.input_str.indexOf("//");
     let ei: number = this.input_str.indexOf("\n");
 
     // Handling different delimiters of any length
     if (si != -1) {
       //Multiple delimiters with brackets
-      if(this.input_str[si + 2] == "["){
+      if (this.input_str[si + 2] == "[") {
         this.handleMultipleDifferentDelimiters(si, ei);
       }
       //Single delimiter without brackets
-      else{
+      else {
         //changing the delimiter
         delimiter = this.input_str.slice(si + 2, ei);
         // Removing the delimiter prefix part
         this.input_str = this.input_str.substring(ei + 1);
       }
-      console.log("input string is : " + this.input_str);
     }
-    
-
 
     // Handling new line numbers
     this.input_str = this.input_str.replace(/\n/g, delimiter);
 
     //split the string based on delimiter
     const numbers: string[] = this.input_str.split(delimiter);
-    let sum: number = 0; 
+    let sum: number = 0;
     //Negative numbers message
     let negativeNumbersError: string = "";
 
     //calculate the sum of the numbers
-    for(let number of numbers){
-      console.log(number);
-      let value : number = parseInt(number);
+    for (let number of numbers) {
+      let value: number = parseInt(number);
       // handle new lines at end or beginning of string
       if (isNaN(value)) throw Error("Invalid input");
       // handle negative numbers
@@ -78,40 +74,33 @@ class Calculator {
     return sum;
   }
 
-  handleMultipleDifferentDelimiters(si: number, ei: number): void{
+  handleMultipleDifferentDelimiters(si: number, ei: number): void {
     /*
-    ** @description : This function will handle multiple different delimiter and Modify input string
-    ** @param : si : number - start index of delimiter,
-    ** @param : ei : number - end index of delimiter
-    ** @return : void 
-    */
+     ** @description : This function will handle multiple different delimiter and Modify input string
+     ** @param : si : number - start index of delimiter,
+     ** @param : ei : number - end index of delimiter
+     ** @return : void
+     */
 
     //delimiter part of string
-    let delimiter_str : string = this.input_str.substring(si, ei + 1);
+    let delimiter_str: string = this.input_str.substring(si, ei + 1);
     //number part of string
     this.input_str = this.input_str.substring(ei + 1);
 
     //extracting all delimiters from the string using regex
-    let all_delimiters : string[] = delimiter_str.match(/\[(.*?)\]/g) || [];
+    let all_delimiters: string[] = delimiter_str.match(/\[(.*?)\]/g) || [];
 
-    console.log("All delimiters are : " + all_delimiters);
-    
     //extracting the delimiters from the string
     all_delimiters.forEach((delimiter) => {
-    
       //removing the brackets from the delimiter
       delimiter = delimiter.slice(1, -1);
       // convert special characters to escape characters for regex
       let escapedDelimiter: string = delimiter.replace(/[-^$*+?.|]/g, "\\$&");
       //creating a regex pattern for the delimiter
-      let regex : RegExp = new RegExp(escapedDelimiter, "g");
+      let regex: RegExp = new RegExp(escapedDelimiter, "g");
       //replacing the delimiter with comma
       this.input_str = this.input_str.replace(regex, ",");
-    }
-    );
-
-    console.log("After removing delimiters : " + this.input_str);
-
+    });
   }
 }
 export default Calculator;
--- test/stringCalculator.spec.ts
+++ test/stringCalculator.spec.ts
@@ -1,6 +1,5 @@
 import Calculator from "../src/stringCalculator";
 
-
 // function to assert the add function value
 function assertValue(input_string: string, expected: number, message?: string) {
   const calculator = new Calculator(input_string);
@@ -12,81 +11,78 @@ function assertValue(input_string: string, expected: number, message?: string) {
 }
 
 describe("Diffrent count of number into string", () => {
-
   test("Empty string returns zero", () => {
-    assertValue("",0);
-    assertValue("  ",0);
+    assertValue("", 0);
+    assertValue("  ", 0);
   });
 
   test("single number in the string", () => {
-    assertValue("1",1);
+    assertValue("1", 1);
   });
 
   test("two numbers in the string", () => {
-    assertValue("1,2",3);
+    assertValue("1,2", 3);
   });
 
   test("multiple numbers in the string", () => {
-    assertValue("4,22,0,33,53,32,3",147);
+    assertValue("4,22,0,33,53,32,3", 147);
   });
-
 });
 
-describe('Handle new lines in string', () => {
-  test('Handle new line between numbers', () => {
-    assertValue("22\n2,32",56);
+describe("Handle new lines in string", () => {
+  test("Handle new line between numbers", () => {
+    assertValue("22\n2,32", 56);
   });
+
   test("Handle new line at beginning or end of string", () => {
-    assertValue("\n22\n2,32",-1,"Invalid input");
-    assertValue("22\n2,32\n",-1,"Invalid input");
+    assertValue("\n22\n2,32", -1, "Invalid input");
+    assertValue("22\n2,32\n", -1, "Invalid input");
   });
 });
 
-describe('Handle different delimiters', () => {
-  test('Handle different delimiters', () => {
-    assertValue("//;\n1;2",3);
-  });
-  test('Handle different delimiters', () => {
-    assertValue("//.\n3.2.5.6",16);
-  });
-});
-
-describe('Handle negative numbers', () => {
-  test('Handle negative numbers', () => {
-    assertValue("1,-2",-1,"Negatives not allowed : -2");
+describe("Handle different delimiters", () => {
+  test("Handle different delimiters", () => {
+    assertValue("//;\n1;2", 3);
   });
 
-  test('Handle negative numbers', () => {
-    assertValue("//;\n1;-2;3;-4",-1,"Negatives not allowed : -2,-4");
+  test("Handle different delimiters", () => {
+    assertValue("//.\n3.2.5.6", 16);
   });
 });
 
+describe("Handle negative numbers", () => {
+  test("Handle negative numbers", () => {
+    assertValue("1,-2", -1, "Negatives not allowed : -2");
+  });
 
-describe('Ignore number bigger than 1000.', () => {
-  test('Number bigger than 1000?, Ignored.', () => {
-    assertValue("4,1001,2",6);
-    assertValue("//:\n21:2333:5:40: 1000:4 ",1070)
+  test("Handle negative numbers", () => {
+    assertValue("//;\n1;-2;3;-4", -1, "Negatives not allowed : -2,-4");
   });
 });
 
-describe('Handle different delimiters of any length', () => {
-  test('Handle different delimiters of any length', () => {
-    assertValue("//[***]\n1***2***3",6);
-  }
-  );
-  test('Handle different delimiters of any length', () => {
-    assertValue("//[**]\n1**2**3",6);
-  }
-  );  
+describe("Ignore number bigger than 1000.", () => {
+  test("Number bigger than 1000?, Ignored.", () => {
+    assertValue("4,1001,2", 6);
+    assertValue("//:\n21:2333:5:40: 1000:4 ", 1070);
+  });
 });
 
-describe('Handle multiple delimiters', () => {
-  test('Handle multiple delimiters', () => {
-    assertValue("//[*][%]\n1*2%3",6);
-  }
-  );
-  test('Handle multiple delimiters of different length', () => {
-    assertValue("//[**][%%]\n1**2%%3",6);
-  }
-  );  
+describe("Handle different delimiters of any length", () => {
+  test("Handle different delimiters of any length", () => {
+    assertValue("//[***]\n1***2***3", 6);
+  });
+
+  test("Handle different delimiters of any length", () => {
+    assertValue("//[**]\n1**2**3", 6);
+  });
+});
+
+describe("Handle multiple delimiters", () => {
+  test("Handle multiple delimiters", () => {
+    assertValue("//[*][%]\n1*2%3", 6);
+  });
+
+  test("Handle multiple delimiters of different length", () => {
+    assertValue("//[**][%%]\n1**2%%3", 6);
+  });
 });
