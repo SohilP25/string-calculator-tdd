d630675571527cc358b4a5e8c94143cf7c07d2f4
SohilP25 <srpathan254@gmail.com>
2024-05-26 15:52:47 +0530

    Refactor code

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -13,7 +13,9 @@ class Calculator {
 
   add(): number {
     /*
-      @return : number - Sum of number as output will be an Integer
+    ** @description : This function will calculate the sum of the numbers in the string
+    ** @param : input_str : string - Input must be in string
+    ** @return : number - Sum of number as output will be an Integer
     */
 
     // Removing white spaces
@@ -24,11 +26,11 @@ class Calculator {
       return 0;
     }
 
-    let delimiter = ",";
+    let delimiter: string= ",";
 
     //si = start index, ei = end index
-    let si = this.input_str.indexOf("//");
-    let ei = this.input_str.indexOf("\n");
+    let si: number= this.input_str.indexOf("//");
+    let ei: number = this.input_str.indexOf("\n");
 
     // Handling different delimiters of any length
     if (si != -1) {
@@ -52,15 +54,15 @@ class Calculator {
     this.input_str = this.input_str.replace(/\n/g, delimiter);
 
     //split the string based on delimiter
-    const numbers = this.input_str.split(delimiter);
-    let sum = 0; 
+    const numbers: string[] = this.input_str.split(delimiter);
+    let sum: number = 0; 
     //Negative numbers message
-    let negativeNumbersError = "";
+    let negativeNumbersError: string = "";
 
     //calculate the sum of the numbers
     for(let number of numbers){
       console.log(number);
-      let value = parseInt(number);
+      let value : number = parseInt(number);
       // handle new lines at end or beginning of string
       if (isNaN(value)) throw Error("Invalid input");
       // handle negative numbers
@@ -76,14 +78,21 @@ class Calculator {
     return sum;
   }
 
-  handleMultipleDifferentDelimiters(si: number, ei: number){
+  handleMultipleDifferentDelimiters(si: number, ei: number): void{
+    /*
+    ** @description : This function will handle multiple different delimiter and Modify input string
+    ** @param : si : number - start index of delimiter,
+    ** @param : ei : number - end index of delimiter
+    ** @return : void 
+    */
+
     //delimiter part of string
-    let delimiter_str = this.input_str.substring(si, ei + 1);
+    let delimiter_str : string = this.input_str.substring(si, ei + 1);
     //number part of string
     this.input_str = this.input_str.substring(ei + 1);
 
     //extracting all delimiters from the string using regex
-    let all_delimiters = delimiter_str.match(/\[(.*?)\]/g) || [];
+    let all_delimiters : string[] = delimiter_str.match(/\[(.*?)\]/g) || [];
 
     console.log("All delimiters are : " + all_delimiters);
     
@@ -93,9 +102,9 @@ class Calculator {
       //removing the brackets from the delimiter
       delimiter = delimiter.slice(1, -1);
       // convert special characters to escape characters for regex
-      let escapedDelimiter = delimiter.replace(/[-^$*+?.|]/g, "\\$&");
+      let escapedDelimiter: string = delimiter.replace(/[-^$*+?.|]/g, "\\$&");
       //creating a regex pattern for the delimiter
-      let regex = new RegExp(escapedDelimiter, "g");
+      let regex : RegExp = new RegExp(escapedDelimiter, "g");
       //replacing the delimiter with comma
       this.input_str = this.input_str.replace(regex, ",");
     }
